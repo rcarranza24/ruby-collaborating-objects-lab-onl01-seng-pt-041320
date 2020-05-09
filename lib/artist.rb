250d58990c6ee7239dcdf7cb25d23e0e03a7ab77
Rosa Carranza <rcarranza1024@yahoo.com>
2020-05-09 23:10:42 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,7 +6,7 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
-    
+
   end
 
   def add_song(song)
@@ -18,7 +18,7 @@ class Artist
   end
 
   def self.all
-    @@all
+    @@all << self
   end
 
   def self.find_or_create_by_name(artist_name)
