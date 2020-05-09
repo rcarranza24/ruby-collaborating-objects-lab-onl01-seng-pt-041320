139106729e1c6fa5c3f654063b9a392dbac19114
Rosa Carranza <rcarranza1024@yahoo.com>
2020-05-09 23:34:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,8 +6,9 @@ class Song
    end
 
    def self.all
-     @@all
+     @@all << self
    end
+
     def self.new_by_filename(file)
      artist_name = file.split(" - ")[0]
      song_name = file.split(" - ")[1]
